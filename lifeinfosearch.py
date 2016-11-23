004aa3e80b03d951024980f24ef75d2c9cf1ec5e
jackli <jackli5530@126.com>
2016-11-23 17:32:50 +0800

    modify bug

--- lifeinfosearch.py
+++ lifeinfosearch.py
@@ -4,10 +4,11 @@ from placeAPI import placeApi
 #输入查询条件
 query=raw_input("输入查询项:")
 city=raw_input("输入城市:")
-num=int(raw_input("输入查询第几项："))
+page=raw_input("第几页:")
+index=int(raw_input("输入查询第几项,不能大于10："))
 #实例化类
 info=placeApi()
 #将方法中的返回值赋值变量，方法中返回多个值
-name,address,telephone=info.place_api(query,city,num)
+name,address,telephone=info.place_api(query,city,index,page)
 print name,address,telephone
 
--- placeAPI.py
+++ placeAPI.py
@@ -2,20 +2,25 @@
 import urllib2
 import json
 class placeApi:
-    def place_api(self,query,city,num):
+    def place_api(self,query,city,index,num):
         try:
             #对百度地图api发送请求获取json
             url = 'http://api.map.baidu.com/place/v2/search?query=%s&' \
-                  'page_size=10&page_num=0&scope=1&region=%s&output=json&ak=NYFRcc4A4UBQpq5qLdxhTcfx5DKn5C0H' % (query, city)
+                  'page_size=10&page_num=%d&scope=1&region=%s&output=json&ak=NYFRcc4A4UBQpq5qLdxhTcfx5DKn5C0H' \
+                  %(query,int(num),city)
             req = urllib2.Request(url)
             request = urllib2.urlopen(req)
             jsons = request.read()
             #解析返回的json数据
             info = json.loads(jsons)
             #对json数据进行提取
-            name = info['results'][num-1]["name"]
-            address=info['results'][num-1]["address"]
-            telephone=info['results'][num-1]["telephone"]
+            name = info['results'][index-1].get("name")
+            address=info['results'][index-1].get("address")
+            telephone = info['results'][index - 1].get('telephone')
+            if telephone==None:
+                telephone='无'
+            else:
+                telephone=info['results'][index - 1].get('telephone')
             #返回所需信息
             return name,address,telephone
         except Exception as e:
