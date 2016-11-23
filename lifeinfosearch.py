5628c15d3125e2379d658d62e57824b785095fe9
jackli <jackli5530@126.com>
2016-11-23 18:52:30 +0800

    commit new insert

--- lifeinfosearch.py
+++ lifeinfosearch.py
@@ -9,6 +9,6 @@ index=int(raw_input("输入查询第几项,不能大于10："))
 #实例化类
 info=placeApi()
 #将方法中的返回值赋值变量，方法中返回多个值
-name,address,telephone=info.place_api(query,city,index,page)
-print name,address,telephone
+name,address,telephone,overall=info.place_api(query,city,index,page)
+print name,address,telephone,overall
 
--- placeAPI.py
+++ placeAPI.py
@@ -6,7 +6,7 @@ class placeApi:
         try:
             #对百度地图api发送请求获取json
             url = 'http://api.map.baidu.com/place/v2/search?query=%s&' \
-                  'page_size=10&page_num=%d&scope=1&region=%s&output=json&ak=NYFRcc4A4UBQpq5qLdxhTcfx5DKn5C0H' \
+                  'page_size=10&page_num=%d&scope=2&region=%s&output=json&ak=NYFRcc4A4UBQpq5qLdxhTcfx5DKn5C0H' \
                   %(query,int(num),city)
             req = urllib2.Request(url)
             request = urllib2.urlopen(req)
@@ -17,11 +17,12 @@ class placeApi:
             name = info['results'][index-1].get("name")
             address=info['results'][index-1].get("address")
             telephone = info['results'][index - 1].get('telephone')
+            rating=info['results'][index-1].get('detail_info').get('overall_rating')
             if telephone==None:
                 telephone='无'
             else:
                 telephone=info['results'][index - 1].get('telephone')
             #返回所需信息
-            return name,address,telephone
+            return name,address,telephone,rating
         except Exception as e:
             print '发生异常:',e
